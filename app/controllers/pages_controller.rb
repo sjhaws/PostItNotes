257e13d064847bc653704784a4f2acf0118eb142
Jeremy Bird <jbird@jbird-l.corp.instructure.com>
2018-02-20 20:45:18 -0700

    jer's push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  
+  # pages GET pages#index
   def index
     @notes = Page.all
   end
@@ -10,4 +12,44 @@ class PagesController < ApplicationController
   def new
     @note = Page.new
   end
+
+  def edit
+    @note = Page.find(params[:id])
+  end
+
+  # patch
+  def update
+    @note = Page.find(params[:id])
+
+    if @note.update(page_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  # POST pages#create
+  def create
+    @note = Page.new(page_params)
+
+    if @note.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
+
+  private
+
+  def page_params
+    params.require(:page).permit(:subject, :author, :body)
+  end
+
 end
