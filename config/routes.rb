c8eafa65177ee0b291b566596e3fee62ed525c54
Steven Haws <stevenjhaws@gmail.com>
2018-02-21 20:51:05 -0700

    added static home and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root "staticpages#home"
+
     resources :pages
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
